39307ee356ff5d73f5f680622f3779e252fe4215
KarolinaTya <80701688+KarolinaTya@users.noreply.github.com>
2021-06-06 21:28:59 +0100

    added forecast for next days

--- src/app.js
+++ src/app.js
@@ -21,6 +21,33 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+     <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt=""
+            withd="42px"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">18° </span>
+            <span class="weather-forecast-temperature-min">12° </span>
+          </div>
+        </div>
+      `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -79,6 +106,7 @@ function displayCelciusTemperature(event) {
 let celciusTemperature = null;
 
 search("New York");
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
